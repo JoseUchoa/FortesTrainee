f37d59ded0936bc6ba17f43ad090fbf71cb87723
JosephUchoa <jose.g.uchoa@gmail.com>
2021-10-21 18:41:37 -0300

    atividade do dia 21 de outubro

---
+++ Projetos/21 de Outubro/conta.dpr
@@ -0,0 +1,14 @@
+program conta;
+
+uses
+  Vcl.Forms,
+  classeConta in 'classeConta.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
